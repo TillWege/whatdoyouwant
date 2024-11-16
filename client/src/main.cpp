9cb466dec7eb044cbda871d144693a8255645f15
TillWege <tillwege@gmail.com>
2024-11-16 21:29:28 +0100

    implement scrolling

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -6,48 +6,53 @@
 #include "raygui.h"
 #include "stuff.h"
 
-#define screenWidth 1200
-#define screenHeight 900
+#define INITIAL_SCREEN_WIDTH 1200
+#define INITIAL_SCREEN_HEIGHT 900
 
 char password[256] = {0};
 bool passwordCorrect = false;
 Texture2D image;
 
-
 std::vector<std::string> wishes;
 
 void loginScreen()
 {
-    DrawText("Hallu Mausi <3", 100, 200, 40, GRAY);
-    DrawText("Enter Password", 100, 300, 40, GRAY);
-    GuiTextBox({100, 400, 300, 50}, password, 20, true);
-    DrawTexture(image, (screenWidth - (image.width + 50)), screenHeight / 2 - image.height / 2, WHITE);
+	int width = GetScreenWidth();
+	int height = GetScreenHeight();
 
-    GuiSetStyle(DEFAULT, TEXT_SIZE, 40);
-    if (GuiButton({100, 500, 300, 50}, "Login"))
-    {
+	DrawText("Hallu Mausi <3", 100, 200, 40, GRAY);
+	DrawText("Enter Password", 100, 300, 40, GRAY);
+	GuiTextBox({100, 400, 300, 50}, password, 20, true);
+	DrawTexture(image, width - (image.width + 50), height / 2 - image.height / 2, WHITE);
+
+	GuiSetStyle(DEFAULT, TEXT_SIZE, 40);
+	if (GuiButton({100, 500, 300, 50}, "Login"))
+	{
 		auto pw = std::string(password);
-        if (login(pw))
-					{
+		if (login(pw))
+		{
 			passwordCorrect = true;
 			loadWishes(wishes);
 		}
-    }
+	}
 }
 
 char wish[256] = {0};
+Vector2 scroll = {0, 0};
 
-int scrollOffset = 0;
 void drawListScreen()
 {
+	int width = GetScreenWidth();
+	int height = GetScreenHeight();
+
 	DrawTexture(image, 50, 50, WHITE);
 	GuiSetStyle(DEFAULT, TEXT_SIZE, 40);
-	DrawText("Add a Wish", 100, 550, 40, GRAY);
+	DrawText("Add a Wish", 100, height - 350, 40, GRAY);
 
 	GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
-	GuiTextBox({100, 650, 300, 50}, wish, 20, true);
+	GuiTextBox({100, float(height - 250), 300, 50}, wish, 20, true);
 
-	if (GuiButton({100, 750, 300, 50}, "Add Wish"))
+	if (GuiButton({100, float(height - 150), 300, 50}, "Add Wish"))
 	{
 		std::string w = wish;
 		addWish(w);
@@ -56,21 +61,26 @@ void drawListScreen()
 	}
 
 	int itemCount = wishes.size();
-	int visibleItems = (screenHeight - 200) / 50;
 
-	if (itemCount > visibleItems)
-	{
-		int width = GetScreenWidth();
-		scrollOffset = GuiScrollBar({float(width - 100), 100, 20, screenHeight - 200}, scrollOffset, 0, itemCount - visibleItems);
-	}
+	Rectangle bounds = {600, 0, float(width - 600), float(height)};
+	Rectangle content = {0, 0, float(width - 700), float(itemCount * 50) + 150};
 
+	Rectangle view = {0};
+
+	GuiSetStyle(DEFAULT, BACKGROUND_COLOR, 0xFFFFFFFF);
+	GuiSetStyle(DEFAULT, BORDER_WIDTH, 0);
+
+	GuiScrollPanel(bounds, nullptr, content, &scroll, &view);
+
+	BeginScissorMode(bounds.x, bounds.y, bounds.width, bounds.height);
 	for (int i = 0; i < itemCount; i++)
 	{
-		if (i < scrollOffset || i >= scrollOffset + visibleItems) continue;
+		float yPos = 100 + (i * 50) + scroll.y;
+		if (yPos < 100 || yPos >= height - 50) continue;
 
 		auto state = GuiGetState();
 
-		if (GuiButton({600, float(100 + (i - scrollOffset) * 50), 30, 30}, "X"))
+		if (GuiButton({600, yPos, 30, 30}, "X"))
 		{
 			removeWish(i);
 			loadWishes(wishes);
@@ -81,7 +91,7 @@ void drawListScreen()
 		if (i == 0)
 			GuiSetState(STATE_DISABLED);
 
-		if (GuiButton({650, float(100 + (i - scrollOffset) * 50), 30, 30}, "+"))
+		if (GuiButton({650, yPos, 30, 30}, "+"))
 		{
 			moveWish(i, UP);
 			loadWishes(wishes);
@@ -91,51 +101,43 @@ void drawListScreen()
 		if (i + 1 == wishes.size())
 			GuiSetState(STATE_DISABLED);
 
-		if (GuiButton({700, float(100 + (i - scrollOffset) * 50), 30, 30}, "-"))
+		if (GuiButton({700, yPos, 30, 30}, "-"))
 		{
 			moveWish(i, DOWN);
 			loadWishes(wishes);
 		}
 
 		GuiSetState(state);
-		DrawText(wishes[i].c_str(), 750, 100 + (i - scrollOffset) * 50, 30, GRAY);
+		DrawText(wishes[i].c_str(), 750, yPos, 30, GRAY);
 	}
+	EndScissorMode();
 }
 
-
-
 int main() {
-    // Initialize the window
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "Raylib Window Example");
+	SetTargetFPS(60);
+	InitWindow(INITIAL_SCREEN_WIDTH, INITIAL_SCREEN_HEIGHT, "Raylib Window Example");
 
-    // httplib::Client cli("http://localhost:8080");
+	image = LoadTexture("bg.png");
 
-    // auto res = cli.Post("/check_password");
+	// Main game loop
+	while (!WindowShouldClose()) {   // Detect window close button or ESC key
+		// Start drawing
+		BeginDrawing();
+		std::cout << GetFPS() << std::endl;
 
-    // std::cout << res->status << std::endl;
-    // std::cout << res->body << std::endl;
+		ClearBackground(WHITE);
 
+		if(!passwordCorrect)
+			loginScreen();
+		else
+			drawListScreen();
 
-    image = LoadTexture("bg.png");
+		EndDrawing();
+	}
 
-    // Main game loop
-    while (!WindowShouldClose()) {   // Detect window close button or ESC key
-        // Start drawing
-        BeginDrawing();
+	UnloadTexture(image);
+	CloseWindow();
 
-        ClearBackground(WHITE);
-
-        if(!passwordCorrect)
-            loginScreen();
-        else
-            drawListScreen();
-
-        EndDrawing();
-    }
-
-    UnloadTexture(image);
-    CloseWindow();
-
-    return 0;
+	return 0;
 }
